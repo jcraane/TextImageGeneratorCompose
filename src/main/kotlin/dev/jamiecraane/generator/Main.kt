79171aba8dd678ced0d49235e51803afaac4bed4
jamiecraane <jcraane@anwb.nl>
2022-06-07 22:01:43 +0200

    Update sample

--- src/main/kotlin/dev/jamiecraane/generator/Main.kt
+++ src/main/kotlin/dev/jamiecraane/generator/Main.kt
@@ -3,16 +3,16 @@ package dev.jamiecraane.generator
 import dev.jamiecraane.generator.compose.Text
 import dev.jamiecraane.generator.compose.TextImage
 import dev.jamiecraane.generator.compose.VStack
-import dev.jamiecraane.generator.core.TextNode
-import imageexporter.JpegImageWriter
 import imageexporter.PngImageWriter
 import java.io.File
 
 fun main(args: Array<String>) {
     val output = TextImage {
         VStack {
-            for (i in 0..6) {
-                Text("Hello $i")
+            Text("Hello World")
+
+            for (i in 0..3) {
+                Text("Line $i")
             }
         }
     }
